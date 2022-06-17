a46d3991e140b5427a05eeb11a5d42d3ae17e4b6
GrontyLion <grontylion@protonmail.com>
2022-06-17 08:26:08 +0800

    1.0 初始化

---
+++ src/main/java/com/mjl/dfglxtv3/DfglxtV3Application.java
@@ -0,0 +1,13 @@
+package com.mjl.dfglxtv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfglxtV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfglxtV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjl/dfglxtv3/DfglxtV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjl.dfglxtv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfglxtV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
